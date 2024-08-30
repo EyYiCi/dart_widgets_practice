e084cf7a11b71a73259f3a6de70b8e374af2ca98
Alejandra GC <dev.ing.agc@outlook.com>
2024-08-30 12:20:10 -0600

    ej: ListView.builder

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,8 +11,22 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
-        centerTitle: false,
       ),
+      body:const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ListView.builder(
+      itemCount: appMenuItem.length,
+      itemBuilder:(context, index) => Text(appMenuItem[index].title),
+
     );
   }
 }
